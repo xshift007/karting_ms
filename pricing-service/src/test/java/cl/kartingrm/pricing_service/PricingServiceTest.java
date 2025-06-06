94eb9f064087cf500e2a024522e1f568f5407ae1
xshift007 <65930297+xshift007@users.noreply.github.com>
2025-06-05 22:35:13 -0400

    fix reservation pricing url and add test profile

--- pricing-service/src/test/java/cl/kartingrm/pricing_service/PricingServiceTest.java
+++ pricing-service/src/test/java/cl/kartingrm/pricing_service/PricingServiceTest.java
@@ -6,12 +6,14 @@ import cl.kartingrm.pricing_service.service.PricingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PricingServiceTest {
 
     @Autowired
